acbac96691ba31a869fe775a7e2ac61f53c4496e
Lintra12 <uko.don@gmail.com>
2024-05-08 11:16:39 +0300

    task 3 slider added

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,18 @@ $(function () {
   
   var mixer = mixitup('.works__content');
 
+
+});
+
+$(function () {
+
+$('.hero__autoplay').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  arrows: false,
+  pauseOnHover: false,
+  
+});
 });
